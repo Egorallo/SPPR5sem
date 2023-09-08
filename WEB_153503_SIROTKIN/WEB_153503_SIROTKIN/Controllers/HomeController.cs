2dc82f5b8157a6dde4e51f9a956e4f38b12f08f9
Egorallo <sea25042003@gmail.com>
2023-09-08 19:50:20 +0300

    initial commit

---
+++ WEB_153503_SIROTKIN/WEB_153503_SIROTKIN/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153503_SIROTKIN.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
